38aaf646c37b2c2e7f0851198f09c537fa75d9d1
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-14 17:58:05 -0600

    Create row and cell components

--- src/client/ui/components/PlantCard.tsx
+++ src/client/ui/components/PlantCard.tsx
@@ -12,30 +12,35 @@ type PlantCardProps = {
 const PlantCard = ({ plant }: PlantCardProps): JSX.Element => {
   const theme = useTheme();
 
+  const cardSideRootStyles = css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    backface-visibility: hidden;
+    overflow: hidden;
+  `;
+
   return (
     <article
       css={css`
         perspective: 150rem;
         -moz-perspective: 120rem;
-        position: relative;
       `}
     >
       <div
         css={css`
-          background-image: linear-gradient(
-            to bottom right,
-            ${theme.colors.secondary},
-            ${theme.colors.secondaryAccent}
-          );
-          border-radius: ${theme.borderRadius};
-          text-align: center;
-          backface-visibility: hidden;
+          ${cardSideRootStyles}
         `}
       >
         <div css={addBackgroundImage(plant.imageUrl)}> </div>
         <h4 css={headingStyles('h4')}>{plant.commonName}</h4>
       </div>
-      <div>
+      <div
+        css={css`
+          ${cardSideRootStyles}
+        `}
+      >
         <h4 css={headingStyles('h4')}>Scientific Name:</h4>
         <h6>{plant.scientificName}</h6>
         <h4 css={headingStyles('h4')}>Family Name:</h4>
