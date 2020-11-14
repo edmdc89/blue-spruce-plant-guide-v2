592a5dbfee39f764884b521e6eacefa4e9f87a83
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-13 20:01:51 -0600

    Add query to test local state

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,28 +1,11 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { GET_PLANT_PAGE } from '../../config/store/api/queries/plantIndex';
+import { GET_CURRENT_QUIZ } from '../../config/store/api/queries/plantIndex';
 
 const Home = (): JSX.Element => {
-  const { loading, error, data } = useQuery(GET_PLANT_PAGE);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :( </p>;
-
-  console.log(data.plantCatalog);
-
-  return (
-    <>
-      {data.plantCatalog.map((plant: any) => {
-        return (
-          <div key={plant.id}>
-            <p>{plant.commonName}</p>
-            <p>{plant.scientificName}</p>
-            <p>{plant.imageUrl}</p>
-          </div>
-        );
-      })}
-    </>
-  );
+  const { data } = useQuery(GET_CURRENT_QUIZ);
+  console.log(data);
+  return <h2>Hello World</h2>;
 };
 
 export default Home;
