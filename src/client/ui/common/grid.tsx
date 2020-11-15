af003fea06089a99db87099b19fb5a97bdfb86f0
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-14 22:42:30 -0600

    Refactor grid layout component

--- src/client/ui/common/grid.tsx
+++ src/client/ui/common/grid.tsx
@@ -1,34 +1,29 @@
 /** @jsx jsx */
-import { css, jsx, useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 import { remify } from '../common/helpers';
 
 type validColumnNumber = 2 | 3 | 4;
 
 interface GridProps {
   columnNumber: validColumnNumber;
-  rowHeight: number;
-  children: JSX.Element[];
+  maxWidth?: number;
 }
 
-const Grid = ({ columnNumber, children, rowHeight }: GridProps): JSX.Element => {
-  const theme = useTheme();
-  return (
-    <section
-      css={css`
-        display: grid;
-        grid-template-columns: repeat(
-          ${columnNumber},
-          calc((100% - ${remify(theme.gridGap)}) / ${columnNumber})
-        );
-        grid-template-rows: repeat(auto, ${remify(rowHeight)});
-        grid-gap: ${remify(theme.gridGap)};
-        margin: 0 auto;
-        max-width: ${remify(theme.maxWidth)};
-      `}
-    >
-      {children}
-    </section>
+const Grid = styled.section<GridProps>`
+  display: grid;
+  grid-template-columns: repeat(
+    ${(props) => props.columnNumber},
+    calc(
+      (
+          100% - (${(props) => props.columnNumber} - 1) *
+            ${(props) => remify(props.theme.gridGap)}
+        ) / ${(props) => props.columnNumber}
+    )
   );
-};
+  grid-gap: ${(props) => remify(props.theme.gridGap)};
+  margin: 0 auto;
+  max-width: ${(props) =>
+    props.maxWidth ? remify(props.maxWidth) : remify(props.theme.maxWidth)};
+`;
 
 export default Grid;
