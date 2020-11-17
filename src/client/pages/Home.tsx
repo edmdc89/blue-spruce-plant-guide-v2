fd0b829239e49b7683b76ca6a086cc017cc5d9b6
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-16 22:02:06 -0600

    Display four random plants on home page visit

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,11 +1,24 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { GET_CURRENT_QUIZ } from '../../config/store/api/queries/plantIndex';
+import { GET_RANDOM_PLANT_PAGE } from '../../config/store/api/queries/plantIndex';
+import Grid from '../ui/common/grid';
+import { PlantDetails } from '../../types/app';
+import { PlantCard } from '../ui/components';
 
 const Home = (): JSX.Element => {
-  const { data } = useQuery(GET_CURRENT_QUIZ);
-  console.log(data);
-  return <h2>Hello World</h2>;
+  const { loading, error, data } = useQuery(GET_RANDOM_PLANT_PAGE);
+  const randomIndex = Math.floor(Math.random() * 19);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( </p>;
+  return (
+    <Grid columnNumber={4}>
+      {data.randomPlantCatalog
+        .slice(randomIndex, randomIndex + 4)
+        .map((plant: PlantDetails) => (
+          <PlantCard plant={plant} key={plant.id} />
+        ))}
+    </Grid>
+  );
 };
 
 export default Home;
