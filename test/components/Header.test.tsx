9a9d27b06520762a76b2ce9c5bf8201764f97e54
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-18 13:22:58 -0600

    Add two header unit tests

--- test/components/Header.test.tsx
+++ test/components/Header.test.tsx
@@ -3,24 +3,26 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter as Router } from 'react-router-dom';
 import Header from '../../src/client/components/Header';
+import theme from '../../src/client/ui/common/theme';
+import { ThemeProvider } from '@emotion/react';
+
+const MockHeader = (): JSX.Element => (
+  <Router>
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
+  </Router>
+);
 
 describe('App header', () => {
   it('should have a title', () => {
-    const { getByRole } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
+    const { getByRole } = render(<MockHeader />);
     const appTitle = getByRole('heading', { name: 'Blue Spruce Plant Guide' });
     expect(appTitle).toBeInTheDocument();
   });
 
   it('should have a nav bar with two links', () => {
-    const { getByRole } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
+    const { getByRole } = render(<MockHeader />);
     const plantIndexLink = getByRole('link', { name: 'Plant Index' });
     const quizLink = getByRole('link', { name: 'Quiz' });
 
