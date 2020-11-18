b3047fcaf7ad74ffd0192cd19b0830c6a51b3045
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-18 12:31:35 -0600

    Extract babel config into own file to support Jest

--- webpack.config/common.ts
+++ webpack.config/common.ts
@@ -8,17 +8,6 @@ const common: webpack.Configuration = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                { useBuiltIns: 'usage', corejs: { version: 3, proposals: 'true' } },
-              ],
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-            plugins: ['@emotion', '@loadable/babel-plugin'],
-          },
         },
       },
       {
