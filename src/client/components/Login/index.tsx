d5c341fc56de06bb4be4a7fe48364e29217baef8
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-19 14:12:18 -0600

    Add user login mutation to login signup component

--- src/client/components/Login/index.tsx
+++ src/client/components/Login/index.tsx
@@ -4,7 +4,7 @@ import LoginInput from './Input';
 import Submit from './SubmitBtn';
 import Form from './Form';
 import { headingStyles } from '../../ui/typography/headings';
-import { useMutation } from '@apollo/client';
+import { ApolloError, useMutation } from '@apollo/client';
 import { USER_LOGIN } from '../../../config/store/api/user/mutations';
 import { useState } from 'react';
 
@@ -17,7 +17,7 @@ const LoginSingupForm = ({ className }: LoginFormProps): JSX.Element => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
-  const [userLogin, { data }] = useMutation(USER_LOGIN);
+  const [userLogin, { data: loginData, error: loginError }] = useMutation(USER_LOGIN);
 
   const LoginFormMessage = (): JSX.Element => {
     const msg = isLoginView ? 'Not a user?' : 'Already a user?';
@@ -41,17 +41,30 @@ const LoginSingupForm = ({ className }: LoginFormProps): JSX.Element => {
   const clearInputs = (): void => {
     setEmail('');
     setPassword('');
+    if (!isLoginView) {
+      setName('');
+    }
   };
+
+  const handleSubmission = (): void => {
+    if (isLoginView) {
+      userLogin({
+        variables: { email, password },
+      });
+      clearInputs();
+    }
+  };
+
   return (
     <Form
       className={className}
       onSubmit={(e) => {
         e.preventDefault();
-        userLogin({ variables: { email, password } });
-        clearInputs();
+        handleSubmission();
       }}
     >
       <LoginFormMessage />
+      {loginError && <h6>{loginError.message}</h6>}
       {!isLoginView && <LoginInput id="name" label="name" value={name} setValue={setName} />}
       <LoginInput id="email" label="email" value={email} setValue={setEmail} />
       <LoginInput
@@ -62,6 +75,7 @@ const LoginSingupForm = ({ className }: LoginFormProps): JSX.Element => {
         setValue={setPassword}
       />
       <Submit>Submit</Submit>
+      {loginData && <h6>Login Successful!</h6>}
     </Form>
   );
 };
--- src/config/store/api/user/mutations.ts
+++ src/config/store/api/user/mutations.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const USER_LOGIN = gql`
-  mutation UserLogin($emal: EmailAddress!, $password: String!) {
-    userLogin(email: $email, password: $password) {
+  mutation UserLogin($email: EmailAddress!, $password: String!) {
+    userLogIn(email: $email, password: $password) {
       _id
       name
       email
