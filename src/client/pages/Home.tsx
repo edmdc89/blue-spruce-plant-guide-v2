48124a8b98095b9a547d0763b2726aa5904eb190
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-24 15:49:23 -0600

    Navigate to plant index on login

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -7,13 +7,21 @@ import { jsx } from '@emotion/react';
 import gridify from '../ui/mixins/grid';
 import { rowify } from '../ui/mixins/Row';
 import LoginSingupForm from '../components/Login';
+import { IS_LOGGED_IN } from '../../config/store/api/user/queries';
+import { Redirect } from 'react-router-dom';
 
 const Home = (): JSX.Element => {
   const { loading, error, data } = useQuery(GET_RANDOM_PLANT_PAGE);
+  const {
+    data: { isLoggedIn },
+  } = useQuery(IS_LOGGED_IN);
+
   const randomIndex = Math.floor(Math.random() * 9);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :( </p>;
-  return (
+  return isLoggedIn ? (
+    <Redirect to="/plant-index" />
+  ) : (
     <section css={rowify()}>
       <section className="row-cell" css={gridify(2)}>
         {data.randomPlantCatalog
