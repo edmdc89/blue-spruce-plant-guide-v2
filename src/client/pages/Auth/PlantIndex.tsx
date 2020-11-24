4885ef18d773b0a8a1b89637ea2c3ee07b354f11
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-24 15:55:27 -0600

    Redirect home if user is not logged in

--- src/client/pages/Auth/PlantIndex.tsx
+++ src/client/pages/Auth/PlantIndex.tsx
@@ -5,14 +5,21 @@ import { PlantDetails } from '../../../types/app';
 import gridify from '../../ui/mixins/grid';
 import { PlantCard } from '../../components';
 import { jsx } from '@emotion/react';
+import { IS_LOGGED_IN } from '../../../config/store/api/user/queries';
+import { Redirect } from 'react-router-dom';
 
 const PlantIndex = (): JSX.Element => {
   const { loading, error, data } = useQuery(GET_PLANT_PAGE);
+  const {
+    data: { isLoggedIn },
+  } = useQuery(IS_LOGGED_IN);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :( </p>;
 
-  return (
+  return !isLoggedIn ? (
+    <Redirect to="/" />
+  ) : (
     <section css={gridify(4)}>
       {data.plantCatalog.map((plant: PlantDetails) => (
         <PlantCard plant={plant} key={plant.id} />
