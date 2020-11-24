0f7bccd55a9d8a6f215cd36e89624edad417e4e7
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-24 13:39:20 -0600

    Clean up react cookie related code

--- src/config/store/link.ts
+++ src/config/store/link.ts
@@ -1,21 +1,21 @@
 import fetch from 'cross-fetch';
-import { HttpLink, ApolloLink } from '@apollo/client';
+import { HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
   fetch,
 });
 
-const authMiddleware = (authToken: string): ApolloLink =>
-  new ApolloLink((operation, forward) => {
-    if (authToken) {
-      operation.setContext({
-        headers: {
-          authorization: `Bearer ${authToken}`,
-        },
-      });
-    }
-    return forward(operation);
-  });
+// const authMiddleware = (authToken: string): ApolloLink =>
+//   new ApolloLink((operation, forward) => {
+//     if (authToken) {
+//       operation.setContext({
+//         headers: {
+//           authorization: `Bearer ${authToken}`,
+//         },
+//       });
+//     }
+//     return forward(operation);
+//   });
 
-export { httpLink, authMiddleware };
+export default httpLink;
