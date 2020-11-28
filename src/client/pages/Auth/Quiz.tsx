b49d1c6a463cd45a1482c33a39e4df4984252dbf
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-11-27 18:33:02 -0600

    Move to a local use state approach

--- src/client/pages/Auth/Quiz.tsx
+++ src/client/pages/Auth/Quiz.tsx
@@ -1,14 +1,27 @@
 /** @jsx jsx */
+import { useQuery } from '@apollo/client';
 import { jsx } from '@emotion/react';
+import { useState } from 'react';
+import { GET_RANDOM_QUIZ } from '../../../config/store/api/plants/queries';
 import { Loading } from '../../components/Common/Loading';
 import { QuizCard } from '../../components/QuizCard';
-import { useQuizTracker } from '../../components/QuizCard/utils';
+// import { useQuizTracker } from '../../components/QuizCard/utils';
 
 const Quiz = (): JSX.Element => {
-  const { loading, error, quizTracker } = useQuizTracker();
-  if (loading) return <Loading />;
-  if (quizTracker) console.log(quizTracker());
-  return <QuizCard />;
+  const { data, loading, error } = useQuery(GET_RANDOM_QUIZ);
+  const [round, setRound] = useState(0);
+  const [score, setScore] = useState(0);
+  const { plantQuiz } = data || {};
+  if (error) return <h2>{error.message}</h2>;
+  return loading ? (
+    <Loading />
+  ) : (
+    <QuizCard
+      changeRound={() => setRound(round + 1)}
+      scoreHandler={setScore}
+      currentQuestion={plantQuiz[0]}
+    />
+  );
 };
 
 export default Quiz;
