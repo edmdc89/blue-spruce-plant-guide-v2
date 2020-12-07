f69ba1a4a16724a94a7006b9ce516cb5267193bb
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-12-07 16:07:58 -0600

    Style details to plant card

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-// import { Amplify } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import Layout from '../components/Layout';
 import '../styles/main.scss';
+import config from '../config';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
 
 const App = ({ Component, pageProps }): JSX.Element => (
   <Layout>
