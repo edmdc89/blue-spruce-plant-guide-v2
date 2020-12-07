fef444a5e360f494c411f9479cb599c7612405f6
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-12-06 18:44:17 -0600

    Configure amplify for auth with cognito

---
+++ config.ts
@@ -0,0 +1,8 @@
+export default {
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_XV8OIPu1P',
+    APP_CLIENT_ID: '5sl83b1laucld0s4f4jabpl9dd',
+    IDENTITY_POOL_ID: 'us-east-1:b309fe52-5337-4aee-a414-c18ca46aa5cc',
+  },
+};
