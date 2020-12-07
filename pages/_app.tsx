abdad323b0766880b05a0ff6b8b96c4032d4c37d
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-12-06 18:44:17 -0600

    Configure amplify for auth with cognito

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Amplify } from 'aws-amplify';
 import Layout from '../components/Layout';
+import config from '../config';
 import '../styles/main.scss';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
 
 const App = ({ Component, pageProps }): JSX.Element => (
   <Layout>
