f98c48353c7f5af1c6fa516ede7619c73961112e
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-12-07 19:03:46 -0600

    Check for user on load

--- lib/apolloClient/link.ts
+++ lib/apolloClient/link.ts
@@ -5,14 +5,25 @@ const httpLink = new HttpLink({
   uri: 'http://localhost:3000/dev/graphql',
 });
 
-const authMiddleware = () => {
-  return new ApolloLink((operation, forward) => {
-    operation.setContext(({ headers = {} }) => ({
-      headers: {
-        ...headers,
-      },
-    }));
-    return forward(operation);
-  });
+const getSession = async () => {
+  let session;
+  try {
+    session = await Auth.currentSession();
+    console.log(session);
+    return session;
+  } catch (e) {
+    console.log(e);
+  }
 };
-export default authMiddleware().concat(httpLink);
+
+const AuthLink = new ApolloLink((operation, forward) => {
+  getSession();
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+    },
+  }));
+  return forward(operation);
+});
+
+export default AuthLink.concat(httpLink);
